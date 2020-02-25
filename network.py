d3ad728520642d2dbc7aec09eabe9b5dc40c7be1
Valery Senotov <senotovel@gmail.com>
2020-02-25 10:19:50 +0300

    new: broadcasting for new device

--- network.py
+++ network.py
@@ -19,6 +19,7 @@ def init_socket(port: int, flag, host='', timeout=10):
         s.connect((host, port))
     elif: flag == SO_BROADCAST
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        s.bind((host, port))
     else:
         # TODO: add exception 
         return s
@@ -30,25 +31,34 @@ class Broadcaster(QObject):
     Must derive from QObject in order to emit signals, connect slots to other signals, and operate in a QThread.
     """
 
-    sig_status = pyqtSignal(int, int)  # broadcaster id: emitted status()
+    sig_status = pyqtSignal(int, int, str))  # broadcaster id: emitted status()
     sig_msg = pyqtSignal(str)  # message to be shown to user
 
     def __init__(self, port=5550):
         super().__init__()
         self.port = port   
         self.__abort = False
-        self.sckt_bc = init_socket(self.port, SO_BROADCAST)
-        self.sckt_in = init_socket(self.port, SO_BIND)
+        self.sckt = init_socket(self.port, SO_BROADCAST)
+        #self.sckt_bc = init_socket(self.port, SO_BROADCAST)
+        #self.sckt_in = init_socket(self.port, SO_BIND)
         self.msg = socket.gethostbyname(socket.gethostname())
         self.dest = ('<broadcast>', self.port)
 
     def work(self):
 
         while 1:
-            self.sckt.sendto(msg, dest)
+            self.sckt.sendto(self.msg, self.dest)
             try:
+                msg_device, address = self.sckt.recvfrom(8192)
+                print(address)
+                if msg_device[:len(self.msg)] == self.msg:
+                    self.sig_status(1, port, address)
+            except socket.timeout:
+                self.sckt.sendto(self.msg, self.dest)
+            except:
+                pass
 
-            time.sleep(1)
+            #time.sleep(1)
 
     def abort(self):
         self.sig_msg.emit('Broadcaster notified to abort')
